79dcafc36499af1f177437c8a6af1880a79db86a
KarlaLopez96}git config --global user.name KarlaLopez96 <00058615@uca.edu.sv>
2018-06-23 20:16:51 -0600

    "Creación del menú, de la app_bar y creación de los botones search y add en la app_bar."

--- app/src/main/java/com/karla00058615/supply_project/Activities/MainActivity.java
+++ app/src/main/java/com/karla00058615/supply_project/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.karla00058615.supply_project;
+package com.karla00058615.supply_project.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.karla00058615.supply_project.R;
 
 public class MainActivity extends AppCompatActivity {
 
